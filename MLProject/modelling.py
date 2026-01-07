4f87eba17b08187ccec33c5a0ebfd84b1eb0e01b
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-01-07 19:00:12 +0700

    fix modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,21 +3,33 @@ import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import os
 
 def train_model():
-    mlflow.sklearn.autolog()
+    base_path = os.path.dirname(__file__)
+    csv_path = os.path.join(base_path, 'train_preprocessing.csv')
     
-    # Membaca data dari folder yang sama
-    df = pd.read_csv('train_preprocessing.csv')
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"Dataset tidak ditemukan di: {csv_path}")
+
+    df = pd.read_csv(csv_path)
     
     X = df.drop(columns=['Survived'])
     y = df['Survived']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
+    mlflow.set_experiment("Titanic_Experiment")
+
     with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=100)
         model.fit(X_train, y_train)
+        
+        # Log manual
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.sklearn.log_model(model, "model")
+        print(f"Model trained with accuracy: {accuracy}")
 
 if __name__ == "__main__":
     train_model()
