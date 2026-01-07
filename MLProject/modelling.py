ad05f5cd48875f53873fa94b037aae2acb1c66b2
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-01-07 19:22:09 +0700

    fix modelling + main.yml

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,23 +6,27 @@ from sklearn.ensemble import RandomForestClassifier
 import os
 
 def train_model():
+    # Setup Path
     base_path = os.path.dirname(__file__)
-    csv_path = os.path.join(base_path, 'train_preprocessing.csv')
+    csv_path = os.path.join(base_path, 'train_preprocessing.csv') # Pastikan nama file csv benar
     
+    # Load Data
     df = pd.read_csv(csv_path)
     X = df.drop(columns=['Survived'])
     y = df['Survived']
-    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model = RandomForestClassifier(n_estimators=100)
-    model.fit(X_train, y_train)
-    
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.sklearn.log_model(model, "model")
-    
-    print(f"Training selesai. Accuracy: {accuracy}")
+    mlflow.set_experiment("Titanic_Experiment")
+
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=100)
+        model.fit(X_train, y_train)
+        
+        # Logging
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.sklearn.log_model(model, "model")
+        print(f"Model trained. Accuracy: {accuracy}")
 
 if __name__ == "__main__":
     train_model()
