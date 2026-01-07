ae5805ac1fbedf4b62babe767fee79434cb41d30
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-01-07 19:03:54 +0700

    fix modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,14 +6,12 @@ from sklearn.ensemble import RandomForestClassifier
 import os
 
 def train_model():
+    # Mengatur Path agar fleksibel
     base_path = os.path.dirname(__file__)
     csv_path = os.path.join(base_path, 'train_preprocessing.csv')
     
-    if not os.path.exists(csv_path):
-        raise FileNotFoundError(f"Dataset tidak ditemukan di: {csv_path}")
-
+    # Baca Dataset
     df = pd.read_csv(csv_path)
-    
     X = df.drop(columns=['Survived'])
     y = df['Survived']
     
@@ -21,15 +19,20 @@ def train_model():
     
     mlflow.set_experiment("Titanic_Experiment")
 
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         model = RandomForestClassifier(n_estimators=100)
         model.fit(X_train, y_train)
         
-        # Log manual
+        # Log secara manual
         accuracy = model.score(X_test, y_test)
         mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_param("n_estimators", 100)
+        
+        # Simpan model ke MLflow
         mlflow.sklearn.log_model(model, "model")
-        print(f"Model trained with accuracy: {accuracy}")
+        
+        print(f"Berhasil! Run ID: {run.info.run_id}")
+        print(f"Accuracy: {accuracy}")
 
 if __name__ == "__main__":
     train_model()
